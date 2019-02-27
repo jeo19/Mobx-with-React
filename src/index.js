246f198cb508e72ce647131ee674f7d37b63305f
nila <terrakkk@outlook.com>
2019-02-27 20:19:19 +0100

    fix:Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
-import { decorate, observable, computed, autorun, transaction } from "mobx";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-class GS25 {
-  basket = [];
-
-  get total() {
-    console.log("calculating..!");
-    // Total calculate the price of object in array inside with Reduce function // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-    return this.basket.reduce((prev, curr) => prev + curr.price, 0);
-  }
-
-  select(name, price) {
-    this.basket.push({ name, price });
-  }
-}
-
-// Apply Mobx function each value by decorate
-decorate(GS25, {
-  basket: observable,
-  total: computed
-});
-
-const gs25 = new GS25();
-autorun(() => gs25.total);
-// alert when add new data
-autorun(() => {
-  if (gs25.basket.length > 0) {
-    console.log(gs25.basket[gs25.basket.length - 1]);
-  }
-});
-
-transaction(() => {
-  gs25.select("Water", 800);
-  gs25.select("Water", 800);
-  gs25.select("Pocachip", 1500);
-});
-
-console.log(gs25.total);
+ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorker();
